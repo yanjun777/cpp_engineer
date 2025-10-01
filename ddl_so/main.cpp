3a07314976b146ab316bba412c08a14da50c87a9
yanjun <yanjunq2@illinois.edu>
2025-10-01 19:38:58 +0800

    dll 和 oppo的笔试

---
+++ ddl_so/main.cpp
@@ -0,0 +1,7 @@
+#include "math.h"
+
+
+int main(){
+
+    return 0;
+}
---
+++ oline_accessment/oppo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void question1(){
+    // 斐波那契数列 -- 按照逻辑来就好
+    // 在全局完成计算和前缀和 否则计算超时 
+
+
+}
+
+int main(){
+    
+
+    return 0; 
+}
